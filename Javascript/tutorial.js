cd096921e85b36e4212127b9e74b5744bcbb0a9a
Jaclyn <junwoo97@nate.com>
2020-06-03 01:39:28 +0900

    javascript tutorial

---
+++ Javascript/tutorial.js
@@ -0,0 +1,16 @@
+var name = "Mark"; // var라는 단어는 변수를 선언할 때 사용하는 Keyward
+
+
+var return = '변수명'; // return은 Reserved Words라서 변수명으로 사용할 수 없음
+
+// Future Reserved Keywords 도 있음
+
+/*-----------------------------------------------------------*/
+
+var name = 'Mark'; // name 이 식별자
+function hell() {} // hell()이 식별자
+var person = {name: 'Mark', age: 37}; // person, name, age 모두 식별자
+var 안녕;// 식별자는 한글도 가능, 숫자로 시작하는 건 불가, 공백문자도 불가
+
+
+
